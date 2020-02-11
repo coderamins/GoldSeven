d2cf50c723e6aeead77e50516b92246675e20ce0
RSevdaP <RSevdaP@DESKTOP-J4P9BPC>
2020-02-11 10:09:48 +0330

    Add project files.

---
+++ GoldSeven.TLBot/Controllers/GoldSevenBotController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.UI.WebControls;
+using Telegram.Bot.Types.Enums;
+
+namespace GoldSeven.TLBot.Controllers
+{
+    public class GoldSevenBotController : ApiController
+    {
+        Telegram.Bot.TelegramBotClient bot;
+        //public GoldSevenBot()
+        //{
+        //    bot = new Telegram.Bot.TelegramBotClient("323@#@");
+        //}
+        //public string v()
+        //{
+        //    FontFamily fontFamily = new FontFamily("Tahoma");
+        //    Font font = new Font(
+        //       fontFamily,
+        //       16,
+        //       FontStyle.Regular,
+        //       GraphicsUnit.Pixel);
+
+        //    Label lab = new Label();
+        //    lab.Text = "Font Bold at 24";
+        //    //lab.Font = new Font("Arial", 20);
+
+        //    bot.SendTextMessageAsync(87689, lab, ParseMode.Default, false, false, 0);
+        //}
+    }
+}
